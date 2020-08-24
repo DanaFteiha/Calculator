8e1ffbc2545ec29ff2962d38fd89709ac1b868a7
DanaFteiha <danafteiha@gmail.com>
2020-08-24 12:27:28 +0300

    Second commit

--- src/main/kotlin/Driver.kt
+++ src/main/kotlin/Driver.kt
@@ -2,19 +2,119 @@
   20/8/2020
   Simple calculator using kotlin
  */
+
 //main function
-fun main(){
+fun main() {
     val calculator = Calculator()
-    var result: Double = 0.0
-    // Reading user input
-    println("Please enter the operation: ADD,SUB,MUL,DIV")
-    val operation: String= readLine()!!
-    println("Please enter the operands")
-    val operands: String= readLine()!!
-    val array=operands.split(" ")
-    val firstNumber: Double= array[0].toDouble()
-    val secondNumber: Double= array[1].toDouble()
-    when (operation){
+    var number = 0
+    var firstNumber = 0
+    var operation = ""
+    var secondNumber = 0
+    var result = 0
+    //read user input
+    println("Enter the equation")
+    val equation: String = readLine()!!
+    //change string into a char array
+    for (char in equation.toCharArray()) {
+        //analysing the equation depending on the char
+        when (char) {
+            '0' -> {
+                //number is more than one digit
+                number = if (number != 0)
+                    (number*10) + calculator.zeroClicked()
+                else
+                    calculator.zeroClicked()
+            }
+            '1' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.oneClicked()
+                else
+                    calculator.oneClicked()
+            }
+            '2' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.twoClicked()
+                else
+                    calculator.twoClicked()
+            }
+            '3' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.threeClicked()
+                else
+                    calculator.threeClicked()
+            }
+            '4' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.fourClicked()
+                else
+                    calculator.fourClicked()
+            }
+            '5' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.fiveClicked()
+                else
+                    calculator.fiveClicked()
+            }
+            '6' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.sixClicked()
+                else
+                    calculator.sixClicked()
+            }
+            '7' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.sevenClicked()
+                else
+                    calculator.sevenClicked()
+            }
+            '8' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.eightClicked()
+                else
+                    calculator.eightClicked()
+            }
+            '9' -> {
+                number = if (number != 0)
+                    (number*10) + calculator.nineClicked()
+                else
+                    calculator.nineClicked()
+            }
+            '+' -> {
+                operation = calculator.plusClicked()
+                firstNumber = number
+                number = 0
+            }
+            '-' -> {
+                operation = calculator.minusClicked()
+                firstNumber = number
+                number = 0
+            }
+            '*' -> {
+                operation = calculator.mulClicked()
+                firstNumber = number
+                number = 0
+            }
+            '/' -> {
+                operation = calculator.divClicked()
+                firstNumber = number
+                number = 0
+            }
+            '=' -> {
+                var equalOperation = calculator.equalClicked()
+                secondNumber = number
+                number = 0
+                result = calculateOperation(calculator,firstNumber,secondNumber,operation)
+                println("The result: $result")
+            }
+        }
+    }
+}
+// calculateOperation function
+// inputs: Two numbers of type Int, calculator,and the operation name
+// outputs: The result of the equation of type Int
+fun calculateOperation(calculator : Calculator,firstNumber : Int, secondNumber : Int, operation : String) : Int{
+    var result = 0
+    when (operation) {
         "ADD" -> {
             result = calculator.doOperation(Operation.ADD, firstNumber, secondNumber)
         }
@@ -31,5 +131,5 @@ fun main(){
             println("Enter one of the four operations only")
         }
     }
-    println("The result: $result")
+    return result
 }
